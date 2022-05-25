a5df4058b99028b29609177db33a837006db5db7
homerioni <homerion13@gmail.com>
2022-05-25 14:31:32 +0300

    create header

--- js/main.js
+++ js/main.js
@@ -2,7 +2,13 @@
 
 $(document).ready(function () {
 
+    // Like in product
     $('.like').click(function () {
+        $(this).toggleClass('active');
+    });
+
+    // Burger menu
+    $('.header__burger').click(function () {
         $(this).toggleClass('active');
     });
 
