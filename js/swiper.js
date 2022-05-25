149ca96ea8a3e50e6099a5137d405aa88332ce84
homerioni <homerion13@gmail.com>
2022-05-25 19:59:47 +0300

    create section main-intro

--- js/swiper.js
+++ js/swiper.js
@@ -26,6 +26,47 @@ const rem = function (rem) {
         return 0.005208335 * $(window).width() * rem;
     } else {
         // где 375 это ширина моб версии макета
-        return (100/375) * (0.1 * $(window).width()) * rem;
+        return (100/414) * (0.1 * $(window).width()) * rem;
     }
 }
+
+const main_intro_slider = new Swiper('.main-intro__slider', {
+    direction: 'horizontal',
+    loop: true,
+
+    breakpoints: {
+        769: {
+            slidesPerView: 1.25,
+        },
+        0: {
+            slidesPerView: 1,
+        },
+    },
+
+    pagination: {
+        el: '.main-intro__pagination-bullet',
+        type: 'bullets',
+    },
+
+    autoplay: {
+        delay: 7500,
+    },
+
+    on: {
+        beforeInit: function () {
+            let total = $('.main-intro__slide').length;
+            if (total < 10) {
+                total = '0' + total;
+            }
+            $('.main-intro__slide-nav .slider-nav__total').text(total);
+        },
+
+        slideChange: function (slider) {
+            let current = slider.realIndex + 1;
+            if (current < 10) {
+                current = '0' + current;
+            }
+            $('.main-intro__slide-nav .slider-nav__current').text(current);
+        },
+    },
+});
